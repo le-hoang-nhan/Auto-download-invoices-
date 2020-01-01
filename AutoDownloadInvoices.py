5fff1ebdbef6b3e1a86c523e5d1242604f2015d5
Le Hoang Nhan <46989247+le-hoang-nhan@users.noreply.github.com>
2020-01-01 17:29:14 +0100

    Create AutoDownloadInvoices.py

---
+++ AutoDownloadInvoices.py
@@ -0,0 +1,27 @@
+#import
+from selenium import webdriver
+
+#read a preprocessed file
+List = open(r"C:\.........\invoices.txt").readlines()
+
+#set up references
+options = webdriver.ChromeOptions()
+options.add_argument("--enable-javascript")
+
+#create driver
+driver = webdriver.Chrome(executable_path=r"C:\NhanLeDocomoDigital\Selenium/chromedriver", options=options)
+
+#access database website
+driver.get("https://........org/")
+UserName = driver.find_element_by_id("UserName")
+UserName.send_keys("..........")
+
+Password = driver.find_element_by_id("Password")
+Password.send_keys(".........")
+
+driver.find_element_by_id("submitbtn").click()
+
+#auto download
+for x in range(len(List)):
+    print(List[x])
+    driver.get(List[x])
